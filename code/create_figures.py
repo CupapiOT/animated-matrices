14a4e8910e855ca1f16a7b5d45fa86dbae0e3eee
CupapiOT <166366567+CupapiOT@users.noreply.github.com>
2024-06-28 17:51:28 +0700

    Create create_figures.py
    Has 2 functions that the main file uses, create_2d_basis_vectors is specifically made to do just that, and create_figures is for more general figures.

---
+++ code/create_figures.py
@@ -0,0 +1,59 @@
+from plotly.graph_objects import Figure, Scatter
+
+
+def create_2d_basis_vectors(
+        basis_vectors: dict[str, list[tuple[int, int] | str]]
+        ):
+    fig = Figure()
+
+    fig.add_trace(Scatter(
+        x=[0, basis_vectors['i-hat'][0][0]],
+        y=[0, basis_vectors['i-hat'][0][1]],
+        mode='lines+markers+text',
+        text=['', 'i-hat'],
+        textposition='top center',
+        line=dict(color='green'),
+        name='i-hat'
+    ))
+
+    fig.add_trace(Scatter(
+        x=[0, basis_vectors['j-hat'][0][0]],
+        y=[0, basis_vectors['j-hat'][0][1]],
+        mode='lines+markers+text',
+        text=['', 'j-hat'],
+        textposition='top center',
+        line=dict(color='red'),
+        name='j-hat'
+    ))
+
+    update_fig_layout(fig)
+    return fig
+
+
+def create_figure(vectors: dict) -> Figure:
+    fig = Figure()
+
+    for name, ((x, y), color) in vectors.items():
+        fig.add_trace(Scatter(
+            x=[0, x],
+            y=[0, y],
+            mode='lines+markers+text',
+            text=['', name],
+            textposition='top center',
+            line=dict(color=color),
+            name=name
+        ))
+
+    update_fig_layout(fig)
+    return fig
+
+
+def update_fig_layout(fig):
+    fig.update_layout(
+        title='Graph',
+        xaxis_title='',
+        yaxis_title='',
+        xaxis=dict(range=[-5, 5]),
+        yaxis=dict(range=[-5, 5], scaleanchor='x'),
+        showlegend=True
+    )
