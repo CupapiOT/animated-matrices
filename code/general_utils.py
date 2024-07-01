cd9153fcbd3a512ade1e0512dda54bbe60157c7f
CupapiOT <166366567+CupapiOT@users.noreply.github.com>
2024-07-01 18:10:08 +0700

    Created `general_utils.py`.
    The new file contains a function that is a generalized version of the function `_set_empty_matrix_inputs_to_zero` located in `main.py`. The function is now named `set_nonetype_to_zero` and is located in the file made in this commit.
    
    Why this was done:
    A function with the exact same purpose as `_set_empty_matrix_inputs_to_zero` would have been used to fix the first bug detailed in the last commit, related to the `add_or_edit_vector` callback-function.
    Instead of creating a brand new function with the same purpose as an already-existing function in the `main.py` file, a new file is made with the function that does the same thing as what the other two would have done.

---
+++ code/general_utils.py
@@ -0,0 +1,15 @@
+def set_nonetype_to_zero(
+        *values: int | float | None,
+) -> tuple | list:
+    """
+    Takes any number of int-type or float-type values and filters them,
+    setting each value to 0 if it is a NoneType.
+
+    Parameter:
+    *values = Any numbers.
+
+    Returns:
+    The same number of values, with each value either staying the same
+    or converted to 0 if it was a NoneType.
+    """
+    return [val if val is not None else 0 for val in values]
