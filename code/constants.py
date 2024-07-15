f8292afcdae5cd8b641303257303a816fbc6cc36
CupapiOT <166366567+CupapiOT@users.noreply.github.com>
2024-07-15 20:45:34 +0800

    Refactor letter definitions.
    Switched from tuples to strings for simplicity.

--- code/constants.py
+++ code/constants.py
@@ -1,12 +1,6 @@
-LOWER_LETTERS = (
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-)
+LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
 
-UPPER_LETTERS = (
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-)
+UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 COLORS = (
     'aqua',
