5394ca387ec35b7eb257e1ac50c93598883bf700
CupapiOT <166366567+CupapiOT@users.noreply.github.com>
2025-04-22 22:26:54 +0800

    Create Vector dataclass in project_types.py.
    Implementation will be in future commits.

--- code/project_types.py
+++ code/project_types.py
@@ -1,8 +1,15 @@
 import numpy as np
+from dataclasses import dataclass
 
-__all__ = ['Vectors', 'Matrix', 'MatrixDict', 'Number']
+__all__ = ['Vectors', 'Matrix', 'MatrixDict', 'Number', 'Vector']
 
 type Number = int | float
-type Vectors = dict[str, list[tuple[int, int] | str]]
+type Vectors = dict[str, Vector]
 type Matrix = np.ndarray
 type MatrixDict = dict[str, Matrix]
+
+@dataclass
+class Vector:
+    """Describes the coordinates and color of one vector."""
+    coords: tuple[Number, Number]
+    color: str
